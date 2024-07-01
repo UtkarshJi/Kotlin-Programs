c7c7c8970e1dbdcaa2712932bb91ad02814c68b7
UtkarshJi <149104418+UtkarshJi@users.noreply.github.com>
2024-07-01 22:17:26 +0530

    Update CONSECUTIVE_ODDS.kt

--- CONSECUTIVE_ODDS.kt
+++ CONSECUTIVE_ODDS.kt
@@ -1,4 +1,4 @@
-class Solution {
+//program toe cclass Solution {
     fun threeConsecutiveOdds(arr: IntArray): Boolean {
         for (i in 0 until arr.size - 2) {
             if (arr[i] % 2 != 0 && arr[i + 1] % 2 != 0 && arr[i + 2] % 2 != 0) {

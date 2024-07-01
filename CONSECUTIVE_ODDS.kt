ad974a86a180a7b4535d8c98c5418a6dade3b0c4
UtkarshJi <149104418+UtkarshJi@users.noreply.github.com>
2024-07-01 22:17:07 +0530

    Create CONSECUTIVE_ODDS.kt

---
+++ CONSECUTIVE_ODDS.kt
@@ -0,0 +1,28 @@
+class Solution {
+    fun threeConsecutiveOdds(arr: IntArray): Boolean {
+        for (i in 0 until arr.size - 2) {
+            if (arr[i] % 2 != 0 && arr[i + 1] % 2 != 0 && arr[i + 2] % 2 != 0) {
+                return true
+            }
+        }
+        return false
+    }
+}
+
+fun main() {
+    println("Enter the size of the array: ")
+    val size = readLine()?.toIntOrNull() ?: return
+
+    val arr = IntArray(size)
+    println("Enter the elements of the array: ")
+    for (i in 0 until size) {
+        arr[i] = readLine()?.toIntOrNull() ?: return
+    }
+
+    val solution = Solution()
+    if (solution.threeConsecutiveOdds(arr)) {
+        println("true")
+    } else {
+        println("false")
+    }
+}

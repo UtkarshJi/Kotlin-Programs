eba06ace4a747f490ecdb2e3185b39d25dde2062
UtkarshJi <149104418+UtkarshJi@users.noreply.github.com>
2024-07-01 22:18:17 +0530

    Update CONSECUTIVE_ODDS.kt

--- CONSECUTIVE_ODDS.kt
+++ CONSECUTIVE_ODDS.kt
@@ -1,4 +1,5 @@
-//program tocheck for consecutive oddclass Solution {
+//program to check for consecutive odd numbers in an array
+class Solution {
     fun threeConsecutiveOdds(arr: IntArray): Boolean {
         for (i in 0 until arr.size - 2) {
             if (arr[i] % 2 != 0 && arr[i + 1] % 2 != 0 && arr[i + 2] % 2 != 0) {

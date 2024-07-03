dcd0896415c9023ad33797a94185b3523c18bd5c
UtkarshJi <149104418+UtkarshJi@users.noreply.github.com>
2024-07-03 23:12:34 +0530

    Update LongestCommonPrefix.kt

--- LongestCommonPrefix.kt
+++ LongestCommonPrefix.kt
@@ -1,4 +1,4 @@
-//program to find Longest Common Prefix
+//Kotlin program to find Longest Common Prefix
 class Solution {
     fun longestCommonPrefix(strs: Array<String>): String {
         if (strs.isEmpty()) return ""
